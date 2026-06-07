9ee991c7df5d0502fff024f8956355dff70d0eba
Marco <modasso23@icloud.com>
2026-06-08 00:28:43 +0200

    Changing input variables and how to compute the thir component

--- decod_ausm.m
+++ decod_ausm.m
@@ -1,18 +1,14 @@
-function [f1,f2,f3] = decod_ausm(rho,u,ener)
+function [phi1,phi2,phi3] = decod_ausm(p,u,h)
 
 global gamma ga gb gc gd ge gf gg gh gi gj % era USE PIGAMMA_PAR
-global h %Nuove variabili globali
 
 %A seconda di quello che dice lo schema, possiamo modificare questa
 %funzione per trovare le grandezze che ci interessano.
 
-p = (ener - .5*rho*u^2)./gb;
-h = p / rho * ga; % ricordarsi che stiamo calcolando h ma non lo usiamo
-%rho=p/h*ga;
-%e=p*gb+.5*rho*u^2;
+rho=p/h*ga;
+h_t = h + u^2/2;
 
-f1=rho*u;
-f2=p+rho*u^2;
-f3=u*(p+ener);
-
+phi1=rho*u;
+phi2=p+rho*u^2;
+phi3=rho*u*h_t;
 end
